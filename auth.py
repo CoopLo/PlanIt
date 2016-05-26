527a1244e6bf85c6a85bb1419d91ee71d8daf829
Cooper <lorsung2@illinois.edu>
2016-05-26 00:47:26 -0500

    Got all necessary API keys

---
+++ auth.py
@@ -0,0 +1,12 @@
+# Contains authorization keys from all necerssary API's
+
+yelpSearchKey = {"Consumer Key": "L7SfOPblzQAX0Iro8AqXVw",
+		"Consumer Secret": "PCv0FK74037_YciGus-bPhco80s",
+		"Token": "yusKILp6XcRzIBujYFlmK1n2Ga7y7xhM",
+		"Token Secret": "mPD5_FAPc07Ffd3ligvyu4cy5kw"}
+
+googleMapsAndroidKey =  "AIzaSyADXyhyDna1Gq4MPR9XS0wk_d1RuhjPpA8"
+
+googleMapsiOSKey = "AIzaSyC1JAnggLiSbfDDPYZGrwtma0Yf5_uwGTU"
+
+googleMapsServerKey = "AIzaSyAcz0_YaTUM9Nab5_CVKqq3SEjM3_lM3uc"
